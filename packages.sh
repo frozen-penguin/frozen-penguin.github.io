6f722ff7dcaafd2930c61b53f5e66667d42ff1e7
System Administrator <root@robs-iphone.lan>
2017-02-19 08:49:34 +1100

    Refreshed repo

---
+++ packages.sh
@@ -0,0 +1,10 @@
+dpkg-deb -Zgzip -b debs/spotty
+dpkg-deb -Zgzip -b debs/bettertextselection
+dpkg-deb -Zgzip -b debs/heartthrob
+dpkg-deb -Zgzip -b debs/dontcorrectme
+dpkg-deb -Zgzip -b debs/emojisuggest
+dpkg-deb -Zgzip -b debs/messagefilter
+dpkg-deb -Zgzip -b debs/softtimer
+dpkg-deb -Zgzip -b debs/predictionshortcuts
+dpkg-deb -Zgzip -b debs/colorgrabber
+
